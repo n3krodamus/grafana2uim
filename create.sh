0f1972a8d65d31ef2e21084a41598e2e7aa23369
N3krodamus <n3krodamus@gmail.com>
2022-08-24 19:56:23 -0300

    Modificado create.sh con instrucciones para git

--- create.sh
+++ create.sh
@@ -3,3 +3,20 @@
 python -m pip install venv
 python -m venv grafana2uim
 source grafana2uim/bin/activate
+
+#######
+…or create a new repository on the command line
+echo "# grafana2uim" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:n3krodamus/grafana2uim.git
+git push -u origin main
+
+####
+…or push an existing repository from the command line
+git remote add origin git@github.com:n3krodamus/grafana2uim.git
+git branch -M main
+git push -u origin main
+
